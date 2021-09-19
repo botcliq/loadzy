a112c3e3d9f2b57618df05609c21e03cc03edc13
aspundir <aspundir@kali>
2021-09-19 01:49:58 -0400

    users code resturctured

---
+++ internal/pkg/users/user.go
@@ -0,0 +1,69 @@
+package user
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/botcliq/loadzy/internal/pkg/action"
+	"github.com/botcliq/loadzy/internal/pkg/feeder"
+	"github.com/botcliq/loadzy/internal/pkg/result"
+	"github.com/botcliq/loadzy/internal/pkg/testdef"
+	"go.uber.org/ratelimit"
+)
+
+func SpawnUsers(t *testdef.TestDef, actions []action.Action) {
+	resultsChannel := make(chan result.HttpReqResult, 10000) // buffer?
+	go result.AcceptResults(resultsChannel)
+	wg := sync.WaitGroup{}
+	rlt := int(t.Rampup / len(t.Actions))
+	rl := ratelimit.New(rlt)
+	for i := 0; i < t.Users; i++ {
+		wg.Add(1)
+		_ = rl.Take()
+		UID := strconv.Itoa(rand.Intn(t.Users+1) + 10000)
+		go launchActions(t, resultsChannel, &wg, actions, UID)
+	}
+	fmt.Println("All users started, waiting at WaitGroup")
+	wg.Wait()
+}
+
+func launchActions(t *testdef.TestDef, resultsChannel chan result.HttpReqResult, wg *sync.WaitGroup, actions []action.Action, UID string) {
+	var sessionMap = make(map[string]string)
+
+	for i := 0; i < t.Iterations; i++ {
+		// Make sure the sessionMap is cleared before each iteration - except for the UID which stays
+		cleanSessionMapAndResetUID(UID, sessionMap)
+		// If we have feeder data, pop an item and push its key-value pairs into the sessionMap
+		feedSession(t, sessionMap)
+		// Iterate over the actions. Note the use of the command-pattern like Execute method on the Action interface
+		for _, action := range actions {
+			if action != nil {
+				go action.Execute(resultsChannel, sessionMap)
+			}
+		}
+		var waitDuration float32 = (float32(t.Users) / float32(t.Rampup)) * float32(len(t.Actions))
+		time.Sleep(time.Duration(int(1000*waitDuration)) * time.Millisecond)
+	}
+	wg.Done()
+}
+
+func cleanSessionMapAndResetUID(UID string, sessionMap map[string]string) {
+	// Optimization? Delete all entries rather than reallocate map from scratch for each new iteration.
+	for k := range sessionMap {
+		delete(sessionMap, k)
+	}
+	sessionMap["UID"] = UID
+}
+
+func feedSession(t *testdef.TestDef, sessionMap map[string]string) {
+	if t.Feeder.Type != "" {
+		go feeder.NextFromFeeder()       // Do async
+		feedData := <-feeder.FeedChannel // Will block here until feeder delivers value over the FeedChannel
+		for item := range feedData {
+			sessionMap[item] = feedData[item]
+		}
+	}
+}
